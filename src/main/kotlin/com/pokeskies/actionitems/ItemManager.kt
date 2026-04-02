2eaa02d557d048d3049fd9a0ef7bc1c670421ec8
Stampede <thestampede2011@gmail.com>
2026-04-02 10:41:11 -0500

    fix: added checks for block place to prevent placing Action Items

--- src/main/kotlin/com/pokeskies/actionitems/ItemManager.kt
+++ src/main/kotlin/com/pokeskies/actionitems/ItemManager.kt
@@ -7,11 +7,13 @@ import com.pokeskies.actionitems.data.UserData
 import com.pokeskies.actionitems.item.ActionItem
 import com.pokeskies.actionitems.utils.TextUtils
 import com.pokeskies.actionitems.utils.Utils
+import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.fabricmc.fabric.api.event.player.UseItemCallback
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.minecraft.core.component.DataComponents
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.item.ItemStack
 import java.util.*
@@ -81,76 +83,90 @@ object ItemManager {
         return true
     }
 
-    private fun registerEvents() {
-        UseItemCallback.EVENT.register(UseItemCallback { p, world, hand ->
-            if (world.isClientSide) return@UseItemCallback InteractionResultHolder.pass(p.getItemInHand(hand))
-            val player = p as ServerPlayer
-            val itemStack = player.getItemInHand(hand)
-            val actionItem = isActionItem(itemStack) ?: return@UseItemCallback InteractionResultHolder.pass(itemStack)
+    private fun onItemUse(player: ServerPlayer, itemStack: ItemStack): InteractionResult {
+        val actionItem = isActionItem(itemStack) ?: return InteractionResult.PASS
 
-            if (interactionCache.contains(player.uuid) && interactionCache[player.uuid]!! > System.currentTimeMillis()) {
-                return@UseItemCallback InteractionResultHolder.fail(itemStack)
-            } else {
-                interactionCache[player.uuid] = System.currentTimeMillis() + ConfigManager.CONFIG.interactionCooldown
-            }
+        if (interactionCache.contains(player.uuid) && interactionCache[player.uuid]!! > System.currentTimeMillis()) {
+            return InteractionResult.FAIL
+        } else {
+            interactionCache[player.uuid] = System.currentTimeMillis() + ConfigManager.CONFIG.interactionCooldown
+        }
 
-            val userData = dataCache.get(player.uuid)
+        val userData = dataCache.get(player.uuid)
 
-            // If cooldowns or limits are set, we need to check user data
-            if (actionItem.cooldown > 0) {
-                userData.cooldowns[actionItem.id]?.let { cooldown ->
-                    if (cooldown + (actionItem.cooldown * 1000) > System.currentTimeMillis()) {
-                        player.sendSystemMessage(
-                            TextUtils.toNative(
-                                "<red>You must wait before using this item again.</red>"
-                            )
-                        )
-                        return@UseItemCallback InteractionResultHolder.fail(itemStack)
-                    }
-                }
-            }
-            if (actionItem.limit > 0) {
-                val usageCount = userData.uses[actionItem.id] ?: 0
-                if (usageCount >= actionItem.limit) {
+        // If cooldowns or limits are set, we need to check user data
+        if (actionItem.cooldown > 0) {
+            userData.cooldowns[actionItem.id]?.let { cooldown ->
+                if (cooldown + (actionItem.cooldown * 1000) > System.currentTimeMillis()) {
                     player.sendSystemMessage(
                         TextUtils.toNative(
-                            "<red>You have reached the usage limit for this item.</red>"
+                            "<red>You must wait before using this item again.</red>"
                         )
                     )
-                    return@UseItemCallback InteractionResultHolder.fail(itemStack)
+                    return InteractionResult.FAIL
                 }
             }
-
-            if (!actionItem.hasRequirements(player)) {
-                return@UseItemCallback InteractionResultHolder.fail(itemStack)
+        }
+        if (actionItem.limit > 0) {
+            val usageCount = userData.uses[actionItem.id] ?: 0
+            if (usageCount >= actionItem.limit) {
+                player.sendSystemMessage(
+                    TextUtils.toNative(
+                        "<red>You have reached the usage limit for this item.</red>"
+                    )
+                )
+                return InteractionResult.FAIL
             }
+        }
 
-            // If cooldowns or limits are set, update user data
-            var dataModified = false
-            if (actionItem.cooldown > 0) {
-                dataModified = true
-                userData.cooldowns[actionItem.id] = System.currentTimeMillis()
+        if (!actionItem.hasRequirements(player)) {
+            return InteractionResult.FAIL
+        }
+
+        // If cooldowns or limits are set, update user data
+        var dataModified = false
+        if (actionItem.cooldown > 0) {
+            dataModified = true
+            userData.cooldowns[actionItem.id] = System.currentTimeMillis()
+        }
+
+        if (actionItem.limit > 0) {
+            dataModified = true
+            val usageCount = userData.uses[actionItem.id] ?: 0
+            userData.uses[actionItem.id] = usageCount + 1
+        }
+
+        if (dataModified) {
+            if (ActionItems.INSTANCE.storage?.saveUser(userData) != true) {
+                Utils.printError("Failed to save user data for ${player.uuid} while attempting to use an Action Item ${actionItem.id}")
+                player.sendMessage(Component.text("Failed to save user data, please contact an admin!").color(NamedTextColor.RED))
+                return InteractionResult.FAIL
             }
+            dataCache.put(player.uuid, userData)
+        }
 
-            if (actionItem.limit > 0) {
-                dataModified = true
-                val usageCount = userData.uses[actionItem.id] ?: 0
-                userData.uses[actionItem.id] = usageCount + 1
-            }
+        if (actionItem.consume) itemStack.shrink(1)
+        actionItem.execute(player)
 
-            if (dataModified) {
-                if (ActionItems.INSTANCE.storage?.saveUser(userData) != true) {
-                    Utils.printError("Failed to save user data for ${player.uuid} while attempting to use an Action Item ${actionItem.id}")
-                    player.sendMessage(Component.text("Failed to save user data, please contact an admin!").color(NamedTextColor.RED))
-                    return@UseItemCallback InteractionResultHolder.fail(itemStack)
-                }
-                dataCache.put(player.uuid, userData)
-            }
+        return InteractionResult.FAIL
+    }
 
-            if (actionItem.consume) itemStack.shrink(1)
-            actionItem.execute(player)
 
-            return@UseItemCallback InteractionResultHolder.pass(itemStack)
-        })
+    private fun registerEvents() {
+        UseItemCallback.EVENT.register { p, level, hand ->
+            val itemStack = p.getItemInHand(hand)
+            if (level.isClientSide) return@register InteractionResultHolder.pass(p.getItemInHand(hand))
+            val player = p as ServerPlayer
+
+            return@register InteractionResultHolder(onItemUse(player, itemStack), itemStack)
+        }
+
+        UseBlockCallback.EVENT.register { p, level, hand, _ ->
+            if (level.isClientSide) return@register InteractionResult.PASS
+            val player = p as ServerPlayer
+            val itemStack = player.getItemInHand(hand)
+
+            return@register onItemUse(player, itemStack)
+        }
     }
 }
